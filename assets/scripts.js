ee76af14b5ea9b5cb9c6ed5b70164530a8a47209
Muhammed Thanish <mnmtanish@gmail.com>
2014-03-22 00:54:36 +0530

    Re-write to support multiple pages

---
+++ assets/scripts.js
@@ -0,0 +1,23 @@
+
+(function () {
+
+  var container = $('#container');
+  var HOMEPAGE = container.html();
+  $(window).on('hashchange', loadPageContent);
+  if (location.hash !== '') loadPageContent();
+
+
+  function loadPageContent () {
+    var link = '/content/'+location.hash.slice(1)+'.md';
+    $.get(link).fail(loadHomepage).done(function (res) {
+      var content = $('<div class="row padded"></div>').html(marked(res))
+      container.empty().append(content);
+    });
+  }
+
+  function loadHomepage () {
+    delete document.body.dataset.twttrRendered;
+    container.html(HOMEPAGE);
+  }
+
+})();
