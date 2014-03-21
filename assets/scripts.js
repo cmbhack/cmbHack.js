4ff20c72a5870adebe8c66ee8534cc20b0ef0e92
Muhammed Thanish <mnmtanish@users.noreply.github.com>
2014-03-22 02:03:10 +0530

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,3 @@
-
 (function () {
 
   var container = $('#container');
@@ -6,18 +5,15 @@
   $(window).on('hashchange', loadPageContent);
   if (location.hash !== '') loadPageContent();
 
-
   function loadPageContent () {
     var link = 'content/'+location.hash.slice(1)+'.md';
-    $.get(link).fail(loadHomepage).done(function (res) {
+    $.get(link).fail(function (res) {
+      delete document.body.dataset.twttrRendered;
+      container.html(HOMEPAGE);
+    }).done(function (res) {
       var content = $('<div class="row padded"></div>').html(marked(res))
       container.empty().append(content);
     });
-  }
-
-  function loadHomepage () {
-    delete document.body.dataset.twttrRendered;
-    container.html(HOMEPAGE);
   }
 
 })();
